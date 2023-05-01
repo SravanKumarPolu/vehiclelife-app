3ec3ff5aa31a976c891b102b8d2ee670f49378f3
PVSKR <skr.polu@hotmail.com>
2023-05-01 14:46:55 +0530

    I am starting VehicleLife app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+     <h1>I am starting VehicleLife app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
