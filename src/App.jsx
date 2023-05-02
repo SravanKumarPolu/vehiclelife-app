281774efd3663ed343a204f4bf92ba58215bac6a
PVSKR <skr.polu@hotmail.com>
2023-05-03 00:16:47 +0530

    Display Van (img,title,type)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter ,Routes,Route,Link} from 'react-router-dom';
 import './App.css';
-
+import Vans from './pages/Vans';
 import Home from './pages/Home';
 import About from './pages/About';
-
+import './server'
 
 
 
@@ -16,11 +16,13 @@ export default function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About </Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
 
       </Routes>
 
---
+++ src/Server.js
@@ -0,0 +1,70 @@
+import { createServer, Model } from "miragejs"
+
+createServer({
+  models: {
+    vans: Model,
+  },
+  seeds(server) {
+    server.create("van", { 
+      id: "1", 
+      name: "Modest Explorer", 
+      price: 2300,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
+      type: "luxury" 
+    })
+    server.create("van", { 
+      id: "2", 
+      name: "Beach Bum", 
+      price: 900,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.", 
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png", 
+      type: "cheap" 
+    })
+    server.create("van", { 
+      id: "3", 
+      name: "Reliable Red", 
+      price: 1100,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png", 
+      type: "comfortable" 
+    })
+
+    server.create("van", { 
+      id: "4", 
+      name: "Dreamfinder", 
+      price: 1700,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png", 
+      type: "simple" 
+    })
+    server.create("van", { 
+      id: "6", 
+      name: "The Cruiser", 
+      price: 1640,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.", 
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png", 
+      type: "luxury" 
+    })
+    server.create("van", { 
+      id: "5", 
+      name: "Green Wonder", 
+      price: 1400,
+      description: "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior, and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+      imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", 
+      type: "rugged" 
+    })
+  },
+  routes() {
+    this.namespace = "api"
+    this.logging=false
+    this.get('/vans', (schema, request) => {
+      return schema.vans.all()
+    })
+
+    this.get('/vans/:id', (schema, request) => {
+      const id = request.params.id
+      return schema.vans.find(id)
+    })
+  }
+})
