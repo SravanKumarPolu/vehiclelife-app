ae800a96fdd6dbcaf55e5ab40358661f3cc094fe
PVSKR <skr.polu@hotmail.com>
2023-05-05 19:18:29 +0530

    Filter the  van type in vans of #VanLife
    Filter the van type in vans of #VanLife with the help of Links

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-import {Link}  from 'react-router-dom'
+import {Link ,useSearchParams}  from 'react-router-dom'
 
 
 export default function Vans(){
+    const [searchParams,setSearchParams]=useSearchParams();
     const [vans,setVans]=React.useState([])
+    const typeFilter=searchParams.get("type")
+    console.log(typeFilter); 
+
     React.useEffect(() => {
        fetch("/api/vans")
    .then(res=>res.json() )
@@ -12,7 +16,10 @@ export default function Vans(){
     }, [])
     console.log(vans)
     
-    const vanElements=vans.map(van=>(
+    const displayedVans=typeFilter
+    ?vans.filter(van=>van.type===typeFilter)
+    :vans
+    const vanElements=displayedVans.map(van=>(
         
      <div key={van.id} className="van-title" >
      <Link to={`/vans/${van.id}`} > 
@@ -29,6 +36,15 @@ export default function Vans(){
     return(
         <div className="van-list-container">
         <h1>Explore our van Options</h1>
+        <div className="van-list-filter-buttons">
+            <nav>
+                <Link to="?type=comfortable" className="van-type-comfort">Comfort</Link>
+                <Link to="?type=luxury"className="van-type-luxury">Luxury</Link>
+                <Link to="?type=rugged"className="van-type-rugged">Rugged</Link>
+                <Link to="?type=simple"className="van-type-simple">Simple</Link>
+            </nav>
+            <Link to="."className="van-type-clear-filter">Clear</Link>
+        </div>
         <div className=" van-list " >
         {vanElements}
         </div>
