de8d041081530579b1ac364519ffecaab378f4d7
PVSKR <skr.polu@hotmail.com>
2023-05-11 13:13:13 +0530

    selected van styling modification

--- src/Server.js
+++ src/Server.js
@@ -64,7 +64,7 @@ createServer({
   routes() {
     this.namespace = "api"
     this.logging=false
-     this.timing=2000;
+     this.timing=1000;
 
     this.get('/vans', (schema, request) => {
       console.log("hiigh")
