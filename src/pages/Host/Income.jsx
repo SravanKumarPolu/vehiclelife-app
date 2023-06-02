d05ecb099409ded72b402a48f1fada0a642a50ce
PVSKR <skr.polu@hotmail.com>
2023-06-02 13:43:50 +0530

    Income bar chart in Host vans

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -1,11 +1,43 @@
 import React from "react"
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-export default function Income(){
-    return(
-        <>
-           
-            <h1>This is Income ...  </h1>
-        </>
-        
+export default function Income() {
+    const transactionsData = [
+        { amount: 7200, date: "Jan 3, '23", id: "1" },
+        { amount: 5600, date: "Dec 12, '22", id: "2" },
+        { amount: 9800, date: "Dec 3, '22", id: "3" },
+    ]
+    return (
+        <section className="host-income">
+            <h1>Income</h1>
+            <p>
+                Last <span>30 days</span>
+            </p>
+            <h2>₹2,2600</h2>
+          
+            <BarChart width={500} height={300} data={transactionsData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="amount" fill="#8884d8" />
+    </BarChart>
+
+            <div className="info-header">
+                <h3>Your transactions (3)</h3>
+                <p>
+                    Last <span>30 days</span>
+                </p>
+            </div>
+            <div className="transactions">
+                {transactionsData.map((item) => (
+                    <div key={item.id} className="transaction">
+                        <h3>₹{item.amount}</h3>
+                        <p>{item.date}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
